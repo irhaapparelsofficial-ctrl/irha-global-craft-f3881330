089e735cf1f65366bbabde8502c96b3e663e8e22
irhaapparelsofficial-ctrl <irhaapparelsofficial@gmail.com>
2026-07-13 23:25:11 +0500

    Integrate Kids Lederhosen catalogue batch

--- src/lib/supplementalCatalogBatch10.ts
+++ src/lib/supplementalCatalogBatch10.ts
@@ -3,6 +3,7 @@ import { createSupplementalBatch10LegacyProductsForSubcategory } from "@/lib/sup
 import { createSupplementalBatch11ProductsForSubcategory } from "@/lib/supplementalCatalogBatch11";
 import { createSupplementalBatch12ProductsForSubcategory } from "@/lib/supplementalCatalogBatch12";
 import { createSupplementalBatch13ProductsForSubcategory } from "@/lib/supplementalCatalogBatch13";
+import { createSupplementalBatch14ProductsForSubcategory } from "@/lib/supplementalCatalogBatch14";
 
 export function createSupplementalBatch10ProductsForSubcategory(
   topCategorySlug: string,
@@ -15,5 +16,6 @@ export function createSupplementalBatch10ProductsForSubcategory(
     ...createSupplementalBatch11ProductsForSubcategory(topCategorySlug, subSlug, subName, categoryId),
     ...createSupplementalBatch12ProductsForSubcategory(topCategorySlug, subSlug, subName, categoryId),
     ...createSupplementalBatch13ProductsForSubcategory(topCategorySlug, subSlug, subName, categoryId),
+    ...createSupplementalBatch14ProductsForSubcategory(topCategorySlug, subSlug, subName, categoryId),
   ];
 }
