a3256d9288de74085e18548720917a7d10ea7f2d
irhaapparelsofficial-ctrl <irhaapparelsofficial@gmail.com>
2026-07-13 22:21:54 +0500

    feat: integrate shipping control into production admin

--- src/components/admin/ProductionWorkflowPanel.tsx
+++ src/components/admin/ProductionWorkflowPanel.tsx
@@ -1,10 +1,12 @@
 import ProductionQualityPanel from "@/components/admin/ProductionQualityPanel";
+import ProductionShippingPanel from "@/components/admin/ProductionShippingPanel";
 import ProductionFactoryWorkflowPanel from "@/components/admin/ProductionFactoryWorkflowPanel";
 
 export default function ProductionWorkflowPanel() {
   return (
     <div className="space-y-6">
       <ProductionQualityPanel />
+      <ProductionShippingPanel />
       <ProductionFactoryWorkflowPanel />
     </div>
   );
