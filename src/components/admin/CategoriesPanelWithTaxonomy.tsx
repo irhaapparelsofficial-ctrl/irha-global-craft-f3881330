cfa79bc229c926194b1e3039e7aa7ae7aecc02c4
irhaapparelsofficial-ctrl <294838484+irhaapparelsofficial-ctrl@users.noreply.github.com>
2026-07-17 03:00:51 +0500

    feat(admin): extend category editor with taxonomy manager

---
+++ src/components/admin/CategoriesPanelWithTaxonomy.tsx
@@ -0,0 +1,11 @@
+import CategoriesPanel from "./CategoriesPanel";
+import TaxonomyPanel from "./TaxonomyPanel";
+
+export default function CategoriesPanelWithTaxonomy() {
+  return (
+    <div>
+      <CategoriesPanel />
+      <TaxonomyPanel />
+    </div>
+  );
+}
